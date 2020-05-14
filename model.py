4c39087ad78a5f8ab783dd1484fa4cf6ef4abe31
胡阳阳 <huyangyang@YANGYANGs-MacBook-Pro.local>
2020-05-13 18:22:42 -0700

    update epoch

--- model.py
+++ model.py
@@ -545,17 +545,19 @@ def train(gen, dis, dataset, epochs, len_low_size, scale, test_dataset=None):
     for epoch in range(epochs):
         start = time.time()
         for i, (low_m, high_m) in enumerate(dataset):
-            if(epoch<1200):
+            if(epoch<=200):
                 loss_weights = [0.0, 10.0, 10.0]
             else:
                 loss_weights = [0.10, 10.0, 10.0]
 
-            if(epoch<450 or (epoch>=1050 and epoch%150<40)):
+            #if(epoch<450 or (epoch>=1050 and epoch%150<40)):
+            if(epoch<1000 or epoch>=2000):
                 train_step_generator(gen, dis, 
                                     tf.dtypes.cast(low_m, tf.float32), tf.dtypes.cast(high_m, tf.float32),
                                     [loss_filter_low, loss_filter_high], loss_weights,
                                     opts, logs)
-            if(epoch%150>=40 or (epoch>=450 and epoch<1050)):
+            #if(epoch%150>=40 or (epoch>=450 and epoch<1050)):
+            if(epoch>=1000):
                 train_step_discriminator(gen, dis, 
                                 tf.dtypes.cast(low_m, tf.float32), tf.dtypes.cast(high_m, tf.float32),
                                 [loss_filter_low, loss_filter_high],
