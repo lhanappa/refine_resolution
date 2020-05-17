bee9b5a4a569a7f42f43ea89195b5a28494c2afa
胡阳阳 <huyangyang@YANGYANGs-MacBook-Pro.local>
2020-05-17 16:20:46 -0700

    new model

--- model.py
+++ model.py
@@ -152,29 +152,24 @@ class Normal(tf.keras.layers.Layer):
 def make_generator_model(len_low_size=16, scale=4):
     In = tf.keras.layers.Input(
         shape=(len_low_size, len_low_size, 1), name='in', dtype=tf.float32)
-    Decl = tf.keras.layers.Conv2D(16, [1, 3], strides=(1,2), padding='valid', data_format="channels_last", 
+    Decl = tf.keras.layers.Conv2D(1024, [1, len_low_size], strides=(1,1), padding='valid', data_format="channels_last", 
                                     activation='relu', use_bias=False,
                                     kernel_constraint=tf.keras.constraints.NonNeg(), 
                                     kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.01, stddev=0.1), 
                                     name='dec_low_1')(In)
-    Decl = tf.keras.layers.Conv2D(128, [1, 3], strides=(1,2), padding='valid', data_format="channels_last", 
+    Decl = tf.keras.layers.Conv2D(128, [1, 3], strides=(1,1), padding='same', data_format="channels_last", 
                                     activation='relu', use_bias=False,
                                     kernel_constraint=tf.keras.constraints.NonNeg(), 
                                     kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.01, stddev=0.1), 
                                     name='dec_low_2')(Decl)
-    Decl = tf.keras.layers.Conv2D(128, [1, 3], strides=(1,2), padding='valid', data_format="channels_last", 
+    Decl = tf.keras.layers.Conv2D(128, [1, 3], strides=(1,1), padding='same', data_format="channels_last", 
                                     activation='relu', use_bias=False,
                                     kernel_constraint=tf.keras.constraints.NonNeg(), 
                                     kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.01, stddev=0.1), 
                                     name='dec_low_3')(Decl)
-    Decl = tf.keras.layers.Conv2D(512, [1, 3], strides=(1,2), padding='valid', data_format="channels_last", 
-                                    activation='relu', use_bias=False,
-                                    kernel_constraint=tf.keras.constraints.NonNeg(), 
-                                    kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.01, stddev=0.1), 
-                                    name='dec_low_4')(Decl)
 
     WeiR1Ml = Weight_R1M(name='WR1Ml')(Decl)
-    Recl = Reconstruct_R1M(512, name='rec_low')(WeiR1Ml)
+    Recl = Reconstruct_R1M(128, name='rec_low')(WeiR1Ml)
     Suml = Sum_R1M(name='sum_low')(Recl)
     low_out = Normal(len_low_size, name='out_low')(Suml)
 
@@ -182,7 +177,7 @@ def make_generator_model(len_low_size=16, scale=4):
     m_F = tf.constant(1/16.0, shape=(1, 1, 1, 1))
     up_o = tf.keras.layers.Multiply(name='scale_value_in')([up_o, m_F])'''
 
-    Rech = Reconstruct_R1M(512, name='rec_high')(WeiR1Ml)
+    Rech = Reconstruct_R1M(128, name='rec_high')(WeiR1Ml)
 
     conv1 = tf.keras.layers.Conv2D(128, [3, 3], strides=1, padding='same', data_format="channels_last", 
                                     activation='relu', use_bias=False,
@@ -199,7 +194,7 @@ def make_generator_model(len_low_size=16, scale=4):
     batchnorm = tf.keras.layers.BatchNormalization()(trans_1)
     sym = Symmetry_R1M(name='SYM_1')(batchnorm)
 
-    trans_2 = Subpixel(filters= int(64), kernel_size=(3,3), r=2, 
+    trans_2 = Subpixel(filters= int(128), kernel_size=(5,5), r=2, 
                         activation='relu', use_bias=False, padding='same', 
                         kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.01, stddev=0.1), 
                         name='subpixel_2')(sym)
@@ -297,7 +292,6 @@ def train_step_generator(Gen, Dis, imgl, imgr, loss_filter, loss_weights, opts, 
         gen_low_v += Gen.get_layer('dec_low_1').trainable_variables
         gen_low_v += Gen.get_layer('dec_low_2').trainable_variables
         gen_low_v += Gen.get_layer('dec_low_3').trainable_variables
-        gen_low_v += Gen.get_layer('dec_low_4').trainable_variables
         gen_low_v += Gen.get_layer('WR1Ml').trainable_variables
         gen_low_v += Gen.get_layer('rec_low').trainable_variables
         gen_low_v += Gen.get_layer('out_low').trainable_variables
