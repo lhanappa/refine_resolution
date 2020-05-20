d024749a9172ab51503cc8e3362c11cfec7420a8
胡阳阳 <huyangyang@YANGYANGs-MacBook-Pro.local>
2020-05-19 17:15:58 -0700

    update model 4

---
+++ utils/operations.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def sampling_hic(hic_matrix, sampling_ratio):
+    """sampling dense hic matrix"""
+    m = np.matrix(hic_matrix)
+    all_sum = m.sum(dtype='float')
+    idx_prob = np.divide(m, all_sum, out=np.zeros_like(m), where=all_sum!=0)
+    idx_prob = np.asarray(idx_prob.reshape((idx_prob.shape[0]*idx_prob.shape[1],)))
+    idx_prob = np.squeeze(idx_prob)
+    sample_number_counts = int(all_sum/sampling_ratio)
+    id_range = np.arange(m.shape[0]*m.shape[1])
+    id_x = np.random.choice(id_range, size=sample_number_counts, replace=True, p=idx_prob)
+    sample_m = np.zeros_like(m)
+    for i in np.arange(sample_number_counts):
+        x = int(id_x[i]/m.shape[0])
+        y = int(id_x[i]%m.shape[0])
+        sample_m[x,y] += 1.0
+    return sample_m
