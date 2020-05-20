f1d363112f32593e24ee1e2264a84aa625ad8c4a
胡阳阳 <huyangyang@YANGYANGs-MacBook-Pro.local>
2020-05-19 22:46:24 -0700

    update model 4 # filters

--- utils/operations.py
+++ utils/operations.py
@@ -7,7 +7,7 @@ def sampling_hic(hic_matrix, sampling_ratio):
     idx_prob = np.divide(m, all_sum, out=np.zeros_like(m), where=all_sum!=0)
     idx_prob = np.asarray(idx_prob.reshape((idx_prob.shape[0]*idx_prob.shape[1],)))
     idx_prob = np.squeeze(idx_prob)
-    sample_number_counts = int(all_sum/sampling_ratio)
+    sample_number_counts = int(all_sum/(2*sampling_ratio))
     id_range = np.arange(m.shape[0]*m.shape[1])
     id_x = np.random.choice(id_range, size=sample_number_counts, replace=True, p=idx_prob)
     sample_m = np.zeros_like(m)
@@ -15,4 +15,5 @@ def sampling_hic(hic_matrix, sampling_ratio):
         x = int(id_x[i]/m.shape[0])
         y = int(id_x[i]%m.shape[0])
         sample_m[x,y] += 1.0
+    sample_m = np.transpose(sample_m) + sample_m
     return sample_m
