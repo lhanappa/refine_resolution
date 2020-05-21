385edd32b13ed289d15b9d60a87c83250c5a43f7
胡阳阳 <huyangyang@YANGYANGs-MacBook-Pro.local>
2020-05-20 18:05:43 -0700

    test dis model

--- model.py
+++ model.py
@@ -651,7 +651,7 @@ def train(gen, dis, dataset, epochs, len_high_size, scale, test_dataset=None):
                 loss_weights = [0.1, 10.0, 10.0]
 
             # if(stage1_gen or (epoch==0 or epoch>=1200 and epoch%100<40)):
-            if(epoch < 500 or epoch >= 1200):
+            if(epoch < 200): # or epoch >= 1200):
                 train_step_generator(gen, dis,
                                      tf.dtypes.cast(low_m, tf.float32), tf.dtypes.cast(
                                          high_m, tf.float32),
