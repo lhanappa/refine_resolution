83a6c5df846acfd3491a86d8be9cd354be00217f
Yangyang Hu <yhu017@ucr.edu>
2020-06-05 13:08:12 -0700

    add bash

---
+++ bash_prepare_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=80G
+#SBATCH --time=1-10:15:00     # 1 day and 10 hours 15 minutes
+#SBATCH --job-name="data"
+#SBATCH -p wmalab
+
+# Print current date
+date
+
+# Load samtools
+#source activate tf_base
+source activate tf_gpu_base
+# run
+python prepare_data.py ${1}
+
+# Print name of node
+hostname
