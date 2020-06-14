66e03faded6160aa43c55830b8092a198114c02b
Yangyang Hu <yhu017@ucr.edu>
2020-06-14 12:41:57 -0700

    update

--- submit_qualify.sh
+++ submit_qualify.sh
@@ -21,4 +21,4 @@ python predict.py
 python qualify.py
 
 source activate 3dchromatin_replicate_qc
-3DChromatin_ReplicateQC run_all --metadata_samples data/output/Rao2014_10kb/SR/chr6/demo.samples  --metadata_pairs data/output/Rao2014_10kb/SR/chr6/demo.pairs --bins data/output/Rao2014_10kb/SR/chr6/demo.bed --outdir data/output/Rao2014_10kb/SR/chr6/ 
+3DChromatin_ReplicateQC run_all --metadata_samples data/output/Rao2014_10kb/SR/chr6/demo.samples  --metadata_pairs data/output/Rao2014_10kb/SR/chr6/demo.pairs --bins data/output/Rao2014_10kb/SR/chr6/demo.bed.gz --outdir data/output/Rao2014_10kb/SR/chr6/ 
