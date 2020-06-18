e6994fc78d3e54f4914f23b1b23a0df84b8b6131
Yangyang Hu <yhu017@ucr.edu>
2020-06-18 12:29:00 -0700

    update dataset

--- submit_training_model.sh
+++ submit_training_model.sh
@@ -2,8 +2,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=80G
-#SBATCH --time=1-10:15:00     # 1 day and 10 hours 15 minutes
+#SBATCH --mem=90G
+#SBATCH --time=20-10:15:00     # 20 day and 10 hours 15 minutes
 #SBATCH --job-name="gan_training"
 ####SBATCH -p wmalab
 #SBATCH -p gpu # This is the default partition, you can use any of the following; intel, batch, highmem, gpu
