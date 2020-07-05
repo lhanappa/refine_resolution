2cf8e1103077fcd15c9e2003f008155e473ea6ad
Yangyang Hu <yhu017@ucr.edu>
2020-07-05 14:35:58 -0700

    update

--- our_model/test_preprocessing.py
+++ our_model/test_preprocessing.py
@@ -2,8 +2,9 @@ import prepare_data
 import sys
 from utils import operations
 import os
-config = prepare_data.configure(len_size=int(sys.argv[2]), genomic_distance=int(sys.argv[3]))
-chromosome_list = [str(sys.argv[1])]
 data_fp = os.path.join(operations.redircwd_back_projroot(project_name='refine_resolution'), 'data')
+config = prepare_data.configure(len_size=int(sys.argv[2]), genomic_distance=int(sys.argv[3]), dataset_path=data_fp)
+chromosome_list = [str(sys.argv[1])]
+
 for chri in chromosome_list:
-    prepare_data.save_samples(config, chromosome=chri, dataset_path=data_fp)
+    prepare_data.save_samples(config, chromosome=chri)
