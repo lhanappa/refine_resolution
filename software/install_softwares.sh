8f32678b7d86b80b704328cfe0aa5548583e1fb3
Yangyang Hu <yhu017@ucr.edu>
2020-07-12 16:52:37 -0700

    update

--- software/install_softwares.sh
+++ software/install_softwares.sh
@@ -4,4 +4,7 @@ echo git clone HiCSR
 git clone https://github.com/PSI-Lab/HiCSR.git
 
 echo git clone deepHiC
-git clone https://github.com/omegahh/DeepHiC.git
+git clone https://github.com/lhanappa/DeepHiC.git
+
+echo git clone hicgan
+git clone https://github.com/lhanappa/hicGAN.git
