7c5f4801f2e93ff937adb32da044013d1b915a5a
Yangyang Hu <yhu017@ucr.edu>
2020-07-12 16:28:47 -0700

    update env

--- software/setup_env.sh
+++ software/setup_env.sh
@@ -12,7 +12,7 @@ if [[ $ENVS = *"${HiCSR}"* ]]; then
    conda env update  --file ./environment_hicsr.yaml  --prune
 else 
    echo "Error: Please provide a valid virtual environment. For a list of valid virtual environment, please see 'conda env list' "
-   echo "Creating env $HiCSR from ./HiCSR/environment.yaml"
+   echo "Creating env $HiCSR from software/environment.yaml"
    echo conda env create -f ./environment_hicsr.yaml
    conda env create --file ./environment_hicsr.yaml
    exit
@@ -29,9 +29,26 @@ if [[ $ENVS = *"${deephic}"* ]]; then
    conda env update  --file ./environment_deephic.yaml  --prune
 else 
    echo "Error: Please provide a valid virtual environment. For a list of valid virtual environment, please see 'conda env list' "
-   echo "Creating env $deephic from ./HiCSR/environment.yaml"
+   echo "Creating env $deephic from software/environment.yaml"
    echo conda env create -f ./environment_deephic.yaml
    conda env create --file ./environment_deephic.yaml
+   exit
+fi;
+
+
+#hicgan
+hicgan=env_hicgan
+ENVS=$(conda env list | awk '{print $1}')
+echo $ENVS
+if [[ $ENVS = *"${hicgan}"* ]]; then
+   echo ${hicgan} exist
+   echo updating
+   conda env update  --file ./environment_hicgan.yaml  --prune
+else 
+   echo "Error: Please provide a valid virtual environment. For a list of valid virtual environment, please see 'conda env list' "
+   echo "Creating env $hicgan from software/environment.yaml"
+   echo conda env create -f ./environment_hicgan.yaml
+   conda env create --file ./environment_hicgan.yaml
    exit
 fi;
 
