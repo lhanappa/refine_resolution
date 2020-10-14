d10a9a85c5b90c1ca0308371c9054a054645ff7d
Yangyang Hu <yhu017@ucr.edu>
2020-10-14 15:26:37 -0700

    update

--- test_preprocessing_hicsr.py
+++ test_preprocessing_hicsr.py
@@ -33,13 +33,13 @@ valid_list = ['17', '18', '22']
  script_work_dir, train_path, train_list, valid_path, valid_list, predict_path, predict_list] = configure_hicsr()
 
 
-"""prepare_hicsr.run(raw_hic=raw_hic,
+prepare_hicsr.run(raw_hic=raw_hic,
                   chromosome_list=chr_list,
                   genomic_distance=genomic_distance,
                   lr_size=lr_size,
                   hr_size=hr_size,
                   downsample_factor=downsample_factor
-                  )"""
+                  )
 
 # python preprocessing.py --input input_samples/ --output preprocessing_output/ --normalize 1
 # input_samples/ --> input_hicsr_2000000_200/Rao2014_GM12878_10kb/
@@ -47,13 +47,13 @@ valid_list = ['17', '18', '22']
 # These sample matrices are stored in the input_samples directory, where each sample has the following naming convention
 # <chromosome>-<cell_type>-<downsample_factor>-<file_tag>.txt.gz
 
-"""if os.path.exists(preprocessing_output_path):
+if os.path.exists(preprocessing_output_path):
     shutil.rmtree(preprocessing_output_path)
 script = "preprocessing.py"
 cmd = ["python", script, "--input", input_path,
        "--output", preprocessing_output_path, "--normalize", "1", "--input_size", str(lr_size), "--output_size", str(hr_size)]
 print(' '.join(cmd))
-process = subprocess.run(cmd, cwd=script_work_dir)"""
+process = subprocess.run(cmd, cwd=script_work_dir)
 
 
 # move train and valid data to cp_path
