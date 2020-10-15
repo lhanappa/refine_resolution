0b547f61fb63150f75b52f5d463499424322f8c2
Yangyang Hu <yhu017@ucr.edu>
2020-10-14 17:45:18 -0700

    update

---
+++ submit_evaluation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=80G
+#SBATCH --time=10-10:15:00     # 10 day and 10 hours 15 minutes
+#SBATCH --job-name="comparison"
+#SBATCH -p gpu # This is the default partition, you can use any of the following; intel, batch, highmem, gpu
+#SBATCH --gres=gpu:k80:1
+#SBATCH --output=slurm-predict-%J.out
+
+# Print current date
+date
+# Print name of node
+hostname
+
+METHOD=${1}
+CHR=${2}
+source activate ipykernel_py3
+echo python test_evaluation.py ${METHOD} ${CHR}
+python test_evaluation.py ${METHOD} ${CHR}
