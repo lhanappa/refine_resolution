8b82cdccb3786622753d10808fbedcbf8a372915
Yangyang Hu <yhu017@ucr.edu>
2020-11-12 13:34:36 -0800

    update

--- our_model/test_preprocessing.py
+++ our_model/test_preprocessing.py
@@ -3,7 +3,8 @@ import sys
 from utils import operations
 import os
 
-raw_hic='Rao2014-GM12878-DpnII-allreps-filtered.10kb.cool'
+raw_hic = 'Rao2014-GM12878-MboI-allreps-filtered.10kb.cool'
+# raw_hic='Rao2014-GM12878-DpnII-allreps-filtered.10kb.cool'
 data_fp = os.path.join(operations.redircwd_back_projroot(project_name='refine_resolution'), 'data')
 config = prepare_data.configure(len_size=int(sys.argv[2]), genomic_distance=int(sys.argv[3]), dataset_path=data_fp, raw_hic=raw_hic)
 chromosome_list = [str(sys.argv[1])]
