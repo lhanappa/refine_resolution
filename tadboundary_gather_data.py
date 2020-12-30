c43537400d567191e588fd63d9a8ce23ccb81f06
Yangyang Hu <yhu017@ucr.edu>
2020-12-30 02:24:52 -0800

    update

--- tadboundary_gather_data.py
+++ tadboundary_gather_data.py
@@ -81,7 +81,7 @@ def generate_cool(input_path='./experiment/tad_boundary', chromosomes=['22', '21
 
         high_mat = mat[num_idx, :]
         high_mat = high_mat[:, num_idx]
-        high_mat = filter_diag_boundary(high_mat, diag_k=2, boundary_k=k)
+        high_mat = filter_diag_boundary(high_mat, diag_k=0, boundary_k=k)
 
         uri = os.path.join(path, hicfile)
         print(uri)
@@ -130,7 +130,6 @@ def generate_cool(input_path='./experiment/tad_boundary', chromosomes=['22', '21
                     mat = scn_recover(mat, dh)'''
                 name = '_'.join([model, win_len])
             mat = filter_diag_boundary(mat, diag_k=0, boundary_k=k)
-            # mat, _ = scn_normalization(mat)
             mat = mat[600:900, 600:900]
             mat = ICE_normalization(mat)
             print('mat shape: {}'.format(mat.shape))
