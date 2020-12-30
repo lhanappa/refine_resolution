6cda2983f03325cea33ff667feb87acee2d69ef5
Yangyang Hu <yhu017@ucr.edu>
2020-12-29 21:18:47 -0800

    update

--- test_tad_boundary.py
+++ test_tad_boundary.py
@@ -90,7 +90,7 @@ def estimate_tad_boundary(chromosomes, models, input_path, output_path=None):
                     "-m", infile, 
                     "--minDepth", "30000",
                     "--maxDepth", "2000000",
-                    "--minBoundaryDistance", "20000"
+                    "--minBoundaryDistance", "20000",
                     "--outPrefix", out,
                     "--thresholdComparisons", "0.05",
                     "--delta", "0.05",
