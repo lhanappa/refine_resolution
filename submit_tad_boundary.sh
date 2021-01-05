30564e2b935a244c8f68e2a68781d5b3d592f587
Yangyang Hu <yhu017@ucr.edu>
2021-01-04 20:49:07 -0800

    add tad boundary script

---
+++ submit_tad_boundary.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+chr=('19' '20' '21' '22' 'X')
+# method=('ours_80' 'ours_200' 'ours_400' 'deephic_40' 'hicsr_40')
+method=('ours_400' 'deephic_40' 'hicsr_40' 'low')
+
+#rm slurm-data-*.out
+for m in "${method[@]}"; do
+    echo sbatch bash_gather_tad_boundary.sh ${m}
+    sbatch bash_gather_tad_boundary.sh ${m}
+done
+
+for c in "${chr[@]}"; do
+    for m in "${method[@]}"; do
+        echo sbatch bash_tad_boundary.sh ${m}
+        sbatch bash_tad_boundary.sh ${m}
+    done
+done
--- tadboundary_gather_data.py
+++ tadboundary_gather_data.py
@@ -166,15 +166,16 @@ overlay_previous = share-y
 if __name__ == '__main__':
     # methods = ['output_ours_2000000_80', 'output_ours_2000000_200', 'output_ours_2000000_400', 'output_hicsr_2000000_40_28', 'output_deephic_2000000_40_40']
     # methods = ['output_ours_2000000_400', 'output_hicsr_2000000_40_28', 'output_deephic_2000000_40_40']
-    methods = ['output_ours_2000000_400']
+    methods = [str(sys.argv[1])]
     
     # cool_file = 'Rao2014-GM12878-DpnII-allreps-filtered.10kb.cool'
     cool_file = 'Rao2014-GM12878-MboI-allreps-filtered.10kb.cool'
     cell_type = cool_file.split('-')[0] + '_' + cool_file.split('-')[1] + '_' + cool_file.split('-')[2] + '_' + cool_file.split('.')[1]
     destination_path = os.path.join('.','experiment', 'tad_boundary', cell_type)
 
-    # chromosomes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X']
-    chromosomes = [ '22' ]
+    chromosomes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X']
+    # chromosomes = [ '22' ]
+    # chromosomes = [str(sys.argv[1])]
     for chro in chromosomes:
         for m in methods:
             source = os.path.join('.', 'data', m, cell_type, 'SR')
