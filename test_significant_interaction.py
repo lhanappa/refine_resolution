fbe6208cdc2a3790f7744a02c5d58ac7f2328fa8
Yangyang Hu <yhu017@ucr.edu>
2021-01-12 20:45:24 -0800

    fix

--- test_significant_interaction.py
+++ test_significant_interaction.py
@@ -250,7 +250,7 @@ def plot_jaccard_score(output_dir, model_js):
     for key, value in model_js.items():
         x = value[:,0]
         y = value[:,1]
-        ax0.scatter(x, y, s=2, label=key)
+        ax0.scatter(x, y, s=5, label=key)
     ax0.legend(loc='upper right', shadow=False)
     fig.tight_layout()
     output = os.path.join(output_dir, 'figure')
