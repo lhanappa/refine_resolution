35ff831ca93c57ec81bd144fade45c4354d5db57
Yangyang Hu <yhu017@ucr.edu>
2021-01-20 15:02:44 -0800

    fix

--- our_model/test_extract_features.py
+++ our_model/test_extract_features.py
@@ -175,7 +175,7 @@ def extract_features(path='./data',
     output = os.path.join(directory_sr, 'prediction_chr{}_{}_{}.png'.format(chromosome, start, end))
     plt.savefig(output, format='png')
 
-    nr,nc = 4,6
+    nr,nc = 6,8
     fig, axs = plt.subplots(nrows=nr, ncols=nc, figsize=(15, 10))
     interm = intermediate_x2.numpy()
     interm = np.squeeze(interm, axis=0)
@@ -186,7 +186,7 @@ def extract_features(path='./data',
     print(interm.shape)
     for i in np.arange(0, nr):
         for j in np.arange(0, nc):
-            idx = (i*nc+j)*3
+            idx = (i*nc+j)*2
             if idx > interm.shape[2]:
                 continue
             m = interm[:,:, idx]
@@ -197,7 +197,7 @@ def extract_features(path='./data',
     output = os.path.join(directory_sr, 'features_x2_chr{}_{}_{}.png'.format(chromosome, start, end))
     plt.savefig(output, format='png')
 
-    nr,nc = 3,5
+    nr,nc = 6,8
     fig, axs = plt.subplots(nrows=nr, ncols=nc, figsize=(15, 10))
     interm = intermediate_x4.numpy()
     interm = np.squeeze(interm, axis=0)
