d3f2f81a4d7cbdbbb0f5bb2e5bb5e363a8fde7a9
Yangyang Hu <yhu017@ucr.edu>
2021-01-20 16:42:39 -0800

    fix

--- test_significant_interaction.py
+++ test_significant_interaction.py
@@ -240,10 +240,9 @@ def plot_significant_interactions(source_dir, chromosome, model_name, resolution
     im = ax0.pcolormesh(X, Y, Z, cmap=cmap, vmin=0, vmax=8)
     fig.colorbar(im, ax=ax0)
 
-    Z = np.zeros_like(hic_mat)
-    Z[si_x, si_y] = 1
-    ax0.pcolormesh(X, Y, Z, cmap=cmap)
-    # ax0.scatter(si_x.flatten(), si_y.flatten(), color="cyan", s=.1)
+    si_x = si_x-0.5
+    si_y = si_y-0.5
+    ax0.scatter(si_x.flatten(), si_y.flatten(), color="cyan", s=.1)
     ax0.set_title('{} log1p Heatmap'.format(model_name))
 
     fig.tight_layout()
