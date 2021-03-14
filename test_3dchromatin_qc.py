c2eb880fbb492b8a366ec39bf45f7ad5b09eb15a
Yangyang Hu <yhu017@ucr.edu>
2021-03-14 14:43:14 -0700

    update

--- test_3dchromatin_qc.py
+++ test_3dchromatin_qc.py
@@ -100,7 +100,7 @@ def run(
             "--metadata_samples",  'metadata_samples.txt', 
             "--bins", 'bins_chr{}.bed.gz'.format(chro), 
             "--outdir", './chromatin_qc/',
-            "--methods", "GenomeDISCO,HiC-Spector,HiCRep",  # HiCRep, ,QuASAR-Rep
+            "--methods", "GenomeDISCO", # HiC-Spector,HiCRep",  # HiCRep, ,QuASAR-Rep
             "--parameters_file", './qc_parameters.txt']
         process.append(subprocess.Popen(cmd, cwd=script_work_dir))
     for p in process:
@@ -116,7 +116,7 @@ def run(
         cmd = ["3DChromatin_ReplicateQC", "concordance", 
             "--metadata_pairs", 'metadata_pairs.txt',
             "--outdir", './chromatin_qc/',
-            "--methods", "GenomeDISCO,HiC-Spector,HiCRep"] # ,QuASAR-Rep
+            "--methods", "GenomeDISCO"] #,HiC-Spector,HiCRep"] # ,QuASAR-Rep
         process.append(subprocess.Popen(cmd, cwd=script_work_dir))
     for p in process:
             p.wait()
