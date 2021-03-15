4e468746a38f1adf4e06a8a51e34269e01845b15
Yangyang Hu <yhu017@ucr.edu>
2021-03-14 23:28:39 -0700

    update

--- test_metrics.py
+++ test_metrics.py
@@ -65,8 +65,8 @@ for mc in metrics:
     data = s.loc[s['metric']==mc]
     data = data.explode('value')
     data['value'] = data['value'].astype('float')
-    fig, ax = plt.figure(figsize=(5,20))
-    sns.catplot(y="cell type", x="value", hue="method", data=data, kind="violin", orient="h", ax=ax)
+    plt.figure(figsize=(5,20))
+    ax = sns.catplot(y="cell type", x="value", hue="method", data=data, kind="violin", orient="h")
     ax.set(xlabel='cell type', ylabel='scores')
     output = os.path.join(output_dir, 'figure')
     os.makedirs(output, exist_ok=True)
