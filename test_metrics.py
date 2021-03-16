c67824c2986cdf48c085894b6e2a7246eaf06c78
Yangyang Hu <yhu017@ucr.edu>
2021-03-16 16:22:04 -0700

    update

--- test_metrics.py
+++ test_metrics.py
@@ -73,7 +73,7 @@ for mc in metrics:
     
     fig, ax = plt.subplots()
     # ax = sns.catplot(y="cell type", x="value", hue="method", data=data, kind="violin", orient="h", height=12, aspect=.8, width=0.8, scale="width", scale_hue=False)
-    g = sns.catplot(ax = ax, row="type", y="cell type", x="value", hue="method", hue_order=methods, 
+    g = sns.catplot(ax = ax, y="cell type", x="value", hue="method", hue_order=methods, 
                     data=data, kind="box", orient="h", height=12, aspect=.9)
     # ax.set(xlabel='cell type', ylabel='scores')
     g.set_axis_labels("Score", "Cell type")
