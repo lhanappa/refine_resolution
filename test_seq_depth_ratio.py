a7a7b071b7ddb46b90b54e754c328c4a8c4f1be3
Yangyang Hu <yhu017@ucr.edu>
2021-03-20 12:08:36 -0700

    update

--- test_seq_depth_ratio.py
+++ test_seq_depth_ratio.py
@@ -72,7 +72,7 @@ for dr in depth_ratio:
                 if me in methods:
                     # GenomeDISCO, HiC-Spector, HiCRep in order
                     for i, mc in enumerate(metrics):
-                        data.append([str(dr), T, me, chro, chrsize[chro], mc, l[i+2]])
+                        data.append([dr, T, me, chro, chrsize[chro], mc, l[i+2]])
         fin.close()
 
 s = pd.DataFrame(data, columns=["ratio", "base", "method", "chromosome", "chromosome length", "metric", "value"])
@@ -86,8 +86,8 @@ for mc in metrics:
     data['value'] = data['value'].astype('float')
     
     fig, ax = plt.subplots()
-    g = sns.catplot(ax = ax, x="ratio", y="value", hue="method", hue_order=methods, 
-                    data=data, kind="box", orient="v", height=12, aspect=1.4)
+    # g = sns.catplot(ax = ax, x="ratio", y="value", hue="method", hue_order=methods, data=data, kind="box", orient="v", height=12, aspect=1.4)
+    g = sns.lineplot(ax=ax, data=data, x="ratio", y="value", hue="method", style="base", markers=True)
     # ax.set(xlabel='cell type', ylabel='scores')
     g.set_axis_labels("Cell type","Score")
     g.set_xticklabels(rotation=30)
