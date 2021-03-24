741b060f2bf20e08a12c4a6dbb45e5a5e3d5a993
Yangyang Hu <yhu017@ucr.edu>
2021-03-24 16:18:07 -0700

    fix

--- response/no4_2/test_seq_lr.py
+++ response/no4_2/test_seq_lr.py
@@ -117,12 +117,13 @@ def split_chrom(path, name, ftype, chrom, resolution):
 
 def prepare():
     chromosomes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X']
-    chromosomes ['22']
+    chromosomes = ['22']
+    resolution = 10000
     for i, chrom in enumerate(chromosomes):
         for j, t in enumerate(list(replication.keys())):
             path = os.path.join('.', t)
             name = 'hic.cool'
-            split_chrom(path, name, t, chrom)
+            split_chrom(path, name, t, chrom, resolution)
 
 if __name__ == '__main__':
     prepare()
