3729866842a8e397acffc5318536ac105fa7adca
Yangyang Hu <yhu017@ucr.edu>
2021-03-24 18:00:01 -0700

    update

--- response/no4_2/prepare_seq_lr.py
+++ response/no4_2/prepare_seq_lr.py
@@ -115,7 +115,7 @@ def split_chrom(path, name, ftype, chrom, resolution):
         hic = sampling_hic(hic, sampling_ratio, fix_seed=True)
         filename = os.path.join(output, 'bed.gz')
         format_bin(hic, resolution=resolution, chrm=chrom, save_file=True, filename=filename)
-    hic = normalization.ICE_normalization(hic)
+    # hic = normalization.ICE_normalization(hic)
     filename = os.path.join(output, '{}_contact.gz'.format(ftype))
     format_contact(hic, resolution=resolution, chrm=chrom, save_file=True, filename=filename)
 
